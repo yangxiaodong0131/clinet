8c54b57c2fb4ecc0a56e252a92a7c3e3e9903d63
tanyawen <tanyawen@hitb.com.cn>
2018-07-09 14:42:47 +0800

    解决删除删除未保存病案同时删掉2018病案问题

--- src/renderer/components/EditPage/LeftBar.vue
+++ src/renderer/components/EditPage/LeftBar.vue
@@ -58,7 +58,7 @@
 
 <script>
   import saveEditDoc from '../../utils/EditSave'
-  import { unSaveFile } from '../../utils/SaveFile'
+  // import { unSaveFile } from '../../utils/SaveFile'
   import { getDocContent } from '../../utils/EditServerFile'
   import { getStat } from '../../utils/StatServerFile'
   import { getLibrary } from '../../utils/LibraryServerFile';
@@ -246,7 +246,7 @@
         if (this.$store.state.Edit.fileName === '未保存病案.cda') {
           // this.$store.commit('EDIT_ADD_DOC', this.$store.state.Edit.doc.toString());
           // // this.$store.commit('EDIT_ADD_DOC', [this.$store.state.Edit.fileIndex, this.$store.state.Edit.doc.toString()]);
-          unSaveFile(this, '2018年度病案.cda', '/edit')
+          // unSaveFile(this, '2018年度病案.cda', '/edit')
         }
       },
       leftEnter(e) {
