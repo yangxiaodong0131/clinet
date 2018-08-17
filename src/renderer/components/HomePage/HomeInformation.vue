78c187dd9433c86109f67ac3463ce538e551825a
tanyawen <tanyawen@hitb.com.cn>
2018-08-17 10:01:55 +0800

    修改Home页样式

--- src/renderer/components/HomePage/HomeInformation.vue
+++ src/renderer/components/HomePage/HomeInformation.vue
@@ -1,224 +1,40 @@
 <template>
   <div class="row">
-    <!-- <div class="row">
-      <div class="col-md-6">
-        <div class="card">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th>待办事项</th>
-                <th>待办时间</th>
-                <th>紧急程度</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="alert alert-danger">
-                <td>待办事项1</td>
-                <td>2018-7-17 16:41:09</td>
-                <td>紧急待办</td>
-              </tr>
-              <tr class="alert alert-warning">
-                <td>待办事项2</td>
-                <td>2018-7-16 16:40:37</td>
-                <td>优先待办</td>
-              </tr>
-              <tr class="alert alert-success">
-                <td>待办事项3</td>
-                <td>2018-7-16 16:40:45</td>
-                <td>普通待办</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div style="">
-          <p class="h3">使用流程</p>
-          <div>
-            <ul class="breadcrumb">
-              <li>
-                  <a href="#">数据采集</a> <span class="divider">></span>
-              </li>
-              <li>
-                  <a href="#">新建自定义文档</a> <span class="divider">></span>
-              </li>
-              <li class="active">查询历史病历</li>
-            </ul>
-          </div>
-          <div>
-            <ul class="breadcrumb">
-              <li>
-                <a href="#">数据分析</a> <span class="divider">></span>
-              </li>
-              <li>
-                <a href="#">表格展示</a> <span class="divider">></span>
-              </li>
-              <li class="active">图表分析</li>
-            </ul>
-          </div>
-          <div>
-            <ul class="breadcrumb">
-              <li>
-                <a href="#">术语字典</a> <span class="divider">></span>
-              </li>
-              <li>
-                <a href="#">表格展示</a> <span class="divider"></span>
-              </li>
-              <li class="active"></li>
-            </ul>
-          </div>
-          <div>
-            <ul class="breadcrumb">
-              <li>
-                <a href="#"></a>系统服务<span class="divider">></span>
-              </li>
-              <li>
-                <a href="#"></a>分组服务<span class="divider">></span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="row">
-          <div class="col-md-12">
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th>数据表</th>
-                  <th>个人数据</th>
-                  <th>上传数据</th>
-                  <th>发布数据</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="alert alert-danger">
-                  <td>CDA文档</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr class="alert alert-danger">
-                  <td>输入框提示</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr class="alert alert-danger">
-                  <td>病案质控</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr class="alert alert-danger">
-                  <td>专家提示</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>        
-        <div role="alert">
-          <h3 class="alert-heading">住院病历</h3>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span><b>入院记录</b></span>
-            </ol>
-          </nav>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span><b>病程记录</b></span>
-              <li v-for="(data, index) in bcRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
-            </ol>
-          </nav>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span><b>围术期相关记录</b></span>
-              <li v-for="(data, index) in wsqRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
-            </ol>
-          </nav>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span><b>出院（死亡）记录</b></span>
-              <li v-for="(data, index) in cyRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
-            </ol>
-          </nav>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span><b>护理相关记录</b></span>
-              <li v-for="(data, index) in hlRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
-            </ol>
-          </nav>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span><b>知情同意书</b></span>
-              <li v-for="(data, index) in zqRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
-            </ol>
-          </nav>
-        </div>
-        <div role="alert">
-          <h3 class="alert-heading">门诊病历</h3>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span></span>
-              <li v-for="(data, index) in mzRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
-            </ol>
-          </nav>
-        </div>
-        <div role="alert">
-          <h3 class="alert-heading">健康体检</h3>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item active" aria-current="page"><a href="#">体检报告</a></li>
-            </ol>
-          </nav>
-        </div>
-        <div role="alert">
-          <h3 class="alert-heading">特殊病历</h3>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <span></span>
-              <li v-for="(data, index) in tsRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
-            </ol>
-          </nav>
-        </div>
-      </div>
-    </div> -->
-    <div class="col-md-6" style="margin-top: 10px">
-      <h3>待办事项</h3>
-      <hr />
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>待办事项</th>
-            <th>待办时间</th>
-            <th>紧急程度</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="alert alert-danger">
-            <td>待办事项1</td>
-            <td>2018-7-17 16:41:09</td>
-            <td>紧急待办</td>
-          </tr>
-          <tr class="alert alert-warning">
-            <td>待办事项2</td>
-            <td>2018-7-16 16:40:37</td>
-            <td>优先待办</td>
-          </tr>
-          <tr class="alert alert-success">
-            <td>待办事项3</td>
-            <td>2018-7-16 16:40:45</td>
-            <td>普通待办</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="col-md-6" style="margin-top: 10px">
-      <h3>使用流程</h3>
-      <hr />
+    <div class="col-md-6" style="marginTop: 10px">
       <div>
-        <ul class="breadcrumb" style>
+        <h3>待办事项</h3>
+        <hr />
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>待办事项</th>
+              <th>待办时间</th>
+              <th>紧急程度</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="alert alert-danger">
+              <td>待办事项1</td>
+              <td>2018-7-17 16:41:09</td>
+              <td>紧急待办</td>
+            </tr>
+            <tr class="alert alert-warning">
+              <td>待办事项2</td>
+              <td>2018-7-16 16:40:37</td>
+              <td>优先待办</td>
+            </tr>
+            <tr class="alert alert-success">
+              <td>待办事项3</td>
+              <td>2018-7-16 16:40:45</td>
+              <td>普通待办</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div style="marginTop: 20px">
+        <h3>使用流程</h3>
+        <hr />
+        <ul class="breadcrumb" style="marginLeft: 0">
           <li>
             <a href="#">数据采集</a> <span class="divider">></span>
           </li>
@@ -227,6 +43,131 @@
           </li>
           <li class="active">查询历史病历</li>
         </ul>
+        <ul class="breadcrumb" style="marginLeft: 0">
+          <li>
+            <a href="#">数据分析</a> <span class="divider">></span>
+          </li>
+          <li>
+            <a href="#">表格展示</a> <span class="divider">></span>
+          </li>
+          <li class="active">图表分析</li>
+        </ul>
+        <ul class="breadcrumb" style="marginLeft: 0">
+          <li>
+            <a href="#">术语字典</a> <span class="divider">></span>
+          </li>
+          <li>
+            <a href="#">表格展示</a> <span class="divider">></span>
+          </li>
+        </ul>
+        <ul class="breadcrumb" style="marginLeft: 0">
+          <li>
+            <a href="#"></a>系统服务<span class="divider">></span>
+          </li>
+          <li>
+            <a href="#"></a>分组服务<span class="divider">></span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-6" style="marginTop: 10px">
+      <div>
+        <h3>数据展示</h3>
+        <hr />
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>数据表</th>
+              <th>个人数据</th>
+              <th>上传数据</th>
+              <th>发布数据</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="alert alert-danger">
+              <td>CDA文档</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr class="alert alert-danger">
+              <td>输入框提示</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr class="alert alert-danger">
+              <td>病案质控</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr class="alert alert-danger">
+              <td>专家提示</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div style="marginTop: 20px">
+        <h3 class="alert-heading">住院病历</h3>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span><b>入院记录</b></span>
+          </ol>
+        </nav>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span><b>病程记录</b></span>
+            <li v-for="(data, index) in bcRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
+          </ol>
+        </nav>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span><b>围术期相关记录</b></span>
+            <li v-for="(data, index) in wsqRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
+          </ol>
+        </nav>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span><b>出院（死亡）记录</b></span>
+            <li v-for="(data, index) in cyRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
+          </ol>
+        </nav>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span><b>护理相关记录</b></span>
+            <li v-for="(data, index) in hlRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
+          </ol>
+        </nav>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span><b>知情同意书</b></span>
+            <li v-for="(data, index) in zqRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
+          </ol>
+        </nav>
+        <h3 class="alert-heading">门诊病历</h3>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span></span>
+            <li v-for="(data, index) in mzRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
+          </ol>
+        </nav>
+        <h3 class="alert-heading">健康体检</h3>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <li class="breadcrumb-item active" aria-current="page"><a href="#">体检报告</a></li>
+          </ol>
+        </nav>
+        <h3 class="alert-heading">特殊病历</h3>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb" style="marginLeft: 0">
+            <span></span>
+            <li v-for="(data, index) in tsRecord" class="breadcrumb-item active" aria-current="page" v-bind:key="index"><a href="#">{{data}}</a></li>
+          </ol>
+        </nav>
       </div>
     </div>
   </div>
