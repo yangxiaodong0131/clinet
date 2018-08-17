c01752aea9b152ad863b9e9283be984f0277183e
duanzhichao <duanzhichao2008@gmail.com>
2018-08-17 16:13:11 +0800

    修复bug

--- src/renderer/components/LibraryPage/RightBar.vue
+++ src/renderer/components/LibraryPage/RightBar.vue
@@ -173,6 +173,7 @@
           this.$store.commit('EDIT_SERVER_FILES', f);
           this.$store.commit('EDIT_SET_FILES_INDEX', 0);
           this.$store.commit('EDIT_SET_LEFT_PANEL', 'table')
+          getLibrary(this, [this.$store.state.System.server, this.$store.state.System.port], this.$store.state.Library.libraryTableInfo.tableName, this.$store.state.Library.libraryTableInfo.page, this.$store.state.Library.dimensionType, this.$store.state.Library.dimensionServer, 'edit', this.$store.state.Library.tableType, this.$store.state.Library.serverSort)
         } else {
           if (this.$store.state.Library.fileIndex !== null) {
             this.$store.commit('EDIT_SET_LEFT_PANEL', 'table');
